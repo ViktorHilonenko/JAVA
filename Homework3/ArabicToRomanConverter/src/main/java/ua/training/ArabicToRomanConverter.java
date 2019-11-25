e4f869442dfce2322f7559bafc79730bcb5fc53e
Victor <viking33@ukr.net>
2019-11-25 13:27:55 +0200

    4 - green

--- Homework3/ArabicToRomanConverter/src/main/java/ua/training/ArabicToRomanConverter.java
+++ Homework3/ArabicToRomanConverter/src/main/java/ua/training/ArabicToRomanConverter.java
@@ -1,13 +1,22 @@
 package ua.training;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ArabicToRomanConverter {
 
     public String convertArabicToRoman(int arabic) {
+        LinkedHashMap<Integer, String> arabicRomanMap = new LinkedHashMap<Integer, String>();
+        arabicRomanMap.put(4, "IV");
+        arabicRomanMap.put(1, "I");
+
         StringBuilder roman = new StringBuilder();
 
-        while (arabic >= 1) {
-            roman.append("I");
-            arabic -= 1;
+        for(Map.Entry<Integer, String> arabicRomanEntry: arabicRomanMap.entrySet()) {
+            while (arabic >= arabicRomanEntry.getKey()) {
+                roman.append(arabicRomanEntry.getValue());
+                arabic -= arabicRomanEntry.getKey();
+            }
         }
 
         return roman.toString();
