8b91e080fb06da132d6938a7d84a01d8cf35165b
Victor <viking33@ukr.net>
2019-11-25 15:26:36 +0200

    JUnit TDD module

---
+++ Homework3/MoreOrLessGame/src/test/java/ua/training/ModelTest.java
@@ -0,0 +1,22 @@
+package ua.training;
+
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.fail;
+
+public class ModelTest {
+    @Test
+    public void generatePuzzledNumber() {
+        fail();
+    }
+
+    @Test
+    public void nextApproach() {
+        fail();
+    }
+
+    @Test
+    public void checkInBoundaries() {
+        fail();
+    }
+}
