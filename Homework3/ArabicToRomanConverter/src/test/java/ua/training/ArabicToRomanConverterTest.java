0460cc9d399bb3b57d0ee693b89a9b916ac1f077
Victor <viking33@ukr.net>
2019-11-25 11:43:02 +0200

    new numbers and tests are red again

--- Homework3/ArabicToRomanConverter/src/test/java/ua/training/ArabicToRomanConverterTest.java
+++ Homework3/ArabicToRomanConverter/src/test/java/ua/training/ArabicToRomanConverterTest.java
@@ -16,4 +16,14 @@ public class ArabicToRomanConverterTest {
     public void convertArabicToRoman_arabic1_romanI() {
         Assert.assertEquals("I", arabicToRomanConverter.convertArabicToRoman(1));
     }
+
+    @Test
+    public void convertArabicToRoman_arabic2_romanII() {
+        Assert.assertEquals("II", arabicToRomanConverter.convertArabicToRoman(2));
+    }
+
+    @Test
+    public void convertArabicToRoman_arabic3_romanIII() {
+        Assert.assertEquals("III", arabicToRomanConverter.convertArabicToRoman(3));
+    }
 }
