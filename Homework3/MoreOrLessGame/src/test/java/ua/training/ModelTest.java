1037bb60534aeef4cf4714b1aaf99fd1547d8f0c
Victor <viking33@ukr.net>
2019-11-25 15:41:55 +0200

    50 is in boundaries function checkInBoundaries has to return True, so now its test is red

--- Homework3/MoreOrLessGame/src/test/java/ua/training/ModelTest.java
+++ Homework3/MoreOrLessGame/src/test/java/ua/training/ModelTest.java
@@ -1,22 +1,25 @@
 package ua.training;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
 import static org.junit.Assert.fail;
 
 public class ModelTest {
+    public static Model model;
+
+    @BeforeClass
+    public static void init() {
+        model = new Model();
+    }
+
     @Test
     public void generatePuzzledNumber() {
         fail();
     }
 
     @Test
-    public void nextApproach() {
-        fail();
-    }
-
-    @Test
-    public void checkInBoundaries() {
-        fail();
+    public void checkInBoundaries_50_is_True() {
+        Assert.assertTrue(model.checkInBoundaries(50));
     }
 }
